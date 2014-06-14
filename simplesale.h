1a2432db2eacd3b47739abf472f63803760d1bc7
Pietro Gagliardi <pietro10@mac.com>
2014-06-14 02:34:15 -0400

    Added GLib/GTK+ versioning macros.

--- simplesale.h
+++ simplesale.h
@@ -1,5 +1,8 @@
 /* 14 june 2014 */
-/* TODO version macros */
+#define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_40
+#define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_40
+#define GDK_VERSION_MIN_REQUIRED GDK_VERSION_3_10
+#define GDK_VERSION_MAX_ALLOWED GDK_VERSION_3_10
 #include <gtk/gtk.h>
 #include <stdint.h>
 #include <stdlib.h>
