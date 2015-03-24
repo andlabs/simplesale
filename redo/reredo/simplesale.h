8c187dad9b3669995f023381ec59a0f907d54161
Pietro Gagliardi <pietro10@mac.com>
2015-03-24 16:50:31 -0400

    Started defining an interface which centralizes all system actions in one place and allows easy mocking/backend changes.

--- redo/reredo/simplesale.h
+++ redo/reredo/simplesale.h
@@ -35,6 +35,57 @@ struct PriceEntryClass {
 };
 extern GType PriceEntry_get_type(void);
 
+// system.c
+#define SystemType (System_get_type())
+#define System(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), SystemType, System))
+#define IsSystem(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), SystemType))
+#define GetSystemInterface(inst) (G_TYPE_INSTANCE_GET_INTERFACE((inst), SystemType, System))
+typedef struct System System;
+struct System {
+	GTypeInterface parent_iface;
+
+#define METHOD(n, ...) (*n)(System *, __VA_ARGS__)
+#define METHOD0(n) (*n)(System *)
+	// TODO shifts and orders
+
+	void METHOD(SetItemsTreeView, GtkTreeView *);
+	void METHOD(SetItemsIconView, GtkIconView *);
+	void METHOD(AddItem, char *, Price);
+	void METHOD(RemoveItem, GtkTreeIter *);
+	char *METHOD(ItemName, GtkTreeIter *);
+	Price METHOD(ItemPrice, GtkTreeIter *);
+	void METHOD(SetItemName, GtkTreeIter *, char *);
+	void METHOD(SetItemPrice, GtkTreeIter *, Price);
+
+	void METHOD(SetEmployeesTreeView, GtkTreeView *);
+	void METHOD(SetEmployeesIconView, GtkIconView *);
+	void METHOD(AddEmployee, char *, char *);
+	void METHOD(RemoveEmployee, GtkTreeIter *);
+	char *METHOD(EmployeeName, GtkTreeIter *);
+	gboolean METHOD(VerifyEmployeePassword, GtkTreeIter *, char *);
+	void METHOD(SetEmployeeName, GtkTreeIter *, char *);
+	void METHOD(ChangeEmployeePassword, GtkTreeIter *, char *, char *);
+
+	// TODO devices
+
+	// TODO tax
+	gboolean METHOD(VerifyManagerPassword, char *);
+	void METHOD(ChangeManagerPassword, char *, char *);
+
+	void METHOD(AddMoney, Price);
+	// TODO really gboolean?
+	gboolean METHOD(RemoveMoney, Price);
+
+	// TODO log?
+	Price METHOD0(CurrentExpectedMoneyInDrawer);
+#undef METHOD
+#undef METHOD0
+};
+extern GType System_get_type(void);
+extern System *system;
+#define sysdo(n, ...) (*(system->n))(system, __VA_ARGS__)
+#define sysdo0(n) (*(system->n))(system)
+
 // manager.c
 extern gboolean manager(void);
 
