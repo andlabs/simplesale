e0cf9619d45497a250e26e7690ba1122ff262092
Pietro Gagliardi <pietro10@mac.com>
2015-03-28 15:25:58 -0400

    Added an About dialog.

--- redo/reredo/manager.c
+++ redo/reredo/manager.c
@@ -37,6 +37,28 @@ static void quitClicked(GtkButton *b, gpointer data)
 	gtk_main_quit();
 }
 
+static const char *authors[] = {
+	"Pietro Gagliardi",
+	NULL,
+};
+
+static void aboutClicked(GtkButton *button, gpointer data)
+{
+	Manager *m = (Manager *) data;
+	GtkWidget *a;
+
+	a = gtk_about_dialog_new();
+	gtk_window_set_transient_for(GTK_WINDOW(a), GTK_WINDOW(m->main));
+	gtk_window_set_modal(GTK_WINDOW(a), TRUE);
+	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(a), "simplesale");		// TODO
+	gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(a), "A dead-simple point of sale system.");
+	gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(a), "Copyright ©2014-2015 Pietro Gagliardi");
+	gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(a), authors);
+	gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(a), GTK_LICENSE_MIT_X11);
+	gtk_dialog_run(GTK_DIALOG(a));
+	gtk_widget_destroy(a);
+}
+
 gboolean manager(void)
 {
 	Manager *m;
@@ -53,6 +75,8 @@ gboolean manager(void)
 		page = (*(pageList[i].new))();
 		gtk_stack_add_titled(GTK_STACK(m->pageStack), page, pageList[i].label, pageList[i].label);
 	}
+
+	g_signal_connect(m->about, "clicked", G_CALLBACK(aboutClicked), m);
 
 	gtk_widget_show_all(m->main);
 	gtk_main();
