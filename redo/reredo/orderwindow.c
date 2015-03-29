895ed993b9dd4b119a3ef7a2d9618cbc188b57d0
Pietro Gagliardi <pietro10@mac.com>
2015-03-28 23:30:44 -0400

    More Order Window stuff and TODOs.

--- redo/reredo/orderwindow.c
+++ redo/reredo/orderwindow.c
@@ -1,6 +1,10 @@
 // 28 march 2015
 #include "simplesale.h"
 
+// TODO
+// - https://git.gnome.org/browse/gedit/tree/gedit/resources/css/gedit-style.css
+// - change the UI to be OrderWindowUI so OrderWindow itself can be a GObject with signals for the shift window
+
 struct OrderWindowPriv {
 	GtkWidget *removeItem;
 };
@@ -19,6 +23,14 @@ void orderwindow(void)
 	g_object_bind_property(o->headerPaned, "position",
 		o->bodyPaned, "position",
 		G_BINDING_DEFAULT | G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+
+	// TODO double-check to make sure Glade doesn't have this
+	actionBar = gtk_action_bar_new();
+	o->priv->removeItem = gtk_button_new_with_label("Remove Item");
+	gtk_action_bar_pack_start(GTK_ACTION_BAR(actionBar), o->priv->removeItem);
+	addNoteButton = gtk_button_new_with_label("Add Note");
+	gtk_action_bar_pack_end(GTK_ACTION_BAR(actionBar), addNoteButton);
+	gtk_container_add(GTK_CONTAINER(o->box1), actionBar);
 
 	gtk_widget_show_all(o->main);
 	gtk_main();
