136d9c8f1b83dd9312011b5cdaf43ba21f29a3f0
Pietro Gagliardi <pietro10@mac.com>
2015-04-01 10:31:48 -0400

    Made AddRemoveMoney a composite widget template/class.

--- redo/reredo/addremovemoney.c
+++ redo/reredo/addremovemoney.c
@@ -1,28 +1,74 @@
 // 24 march 2015
 #include "simplesale.h"
 
-struct AddRemoveMoneyPriv {
+#define AddRemoveMoneyType (AddRemoveMoney_get_type())
+#define AddRemoveMoney(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), AddRemoveMoneyType, AddRemoveMoney))
+#define IsAddRemoveMoney(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), AddRemoveMoneyType))
+#define AddRemoveMoneyClass(class) (G_TYPE_CHECK_CLASS_CAST((class), AddRemoveMoneyType, AddRemoveMoneyClass))
+#define IsAddRemoveMoneyClass(class) (G_TYPE_CHECK_CLASS_TYPE((class), AddRemoveMoney))
+#define GetAddRemoveMoneyClass(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), AddRemoveMoneyType, AddRemoveMoneyClass))
+typedef struct AddRemoveMoney AddRemoveMoney;
+typedef struct AddRemoveMoneyClass AddRemoveMoneyClass;
+struct AddRemoveMoney {
+	GtkGrid parent_instance;
+	struct AddRemoveMoneyPrivate *priv;
+};
+struct AddRemoveMoneyClass {
+	GtkGridClass parent_class;
+};
+static GType AddRemoveMoney_get_type(void);
+
+typedef struct AddRemoveMoneyPrivate AddRemoveMoneyPrivate;
+
+struct AddRemoveMoneyPrivate {
+	GtkWidget *current;
+	GtkWidget *changeAmount;
+	GtkWidget *change;
+	GtkWidget *addAmount;
+	GtkWidget *removeAmount;
 };
 
-#include "zaddremovemoney.h"
+G_DEFINE_TYPE_WITH_CODE(AddRemoveMoney, AddRemoveMoney, GTK_TYPE_GRID,
+	G_ADD_PRIVATE(AddRemoveMoney))
 
-static void destroy(GtkWidget *widget, gpointer data)
+static void AddRemoveMoney_init(AddRemoveMoney *a)
 {
-	AddRemoveMoney *a = (AddRemoveMoney *) data;
+	a->priv = AddRemoveMoney_get_instance_private(a);
 
-	g_free(a);
+	gtk_widget_init_template(GTK_WIDGET(a));
+
+	// Glade doesn't let me set these two
+	gtk_button_set_image(GTK_BUTTON(a->priv->addAmount), gtk_image_new_from_icon_name("list-add-symbolic", GTK_ICON_SIZE_MENU));
+	gtk_button_set_image(GTK_BUTTON(a->priv->removeAmount), gtk_image_new_from_icon_name("list-remove-symbolic", GTK_ICON_SIZE_MENU));
+}
+
+static void AddRemoveMoney_dispose(GObject *obj)
+{
+	G_OBJECT_CLASS(AddRemoveMoney_parent_class)->dispose(obj);
+}
+
+static void AddRemoveMoney_finalize(GObject *obj)
+{
+	G_OBJECT_CLASS(AddRemoveMoney_parent_class)->finalize(obj);
+}
+
+static void AddRemoveMoney_class_init(AddRemoveMoneyClass *class)
+{
+	G_OBJECT_CLASS(class)->dispose = AddRemoveMoney_dispose;
+	G_OBJECT_CLASS(class)->finalize = AddRemoveMoney_finalize;
+
+	// TODO macroize filename somehow?
+	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/simplesale/addremovemoney.ui");
+#define T(name) gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(class), AddRemoveMoney, name)
+	T(current);
+	T(changeAmount);
+	T(change);
+	T(addAmount);
+	T(removeAmount);
+#undef T
 }
 
 GtkWidget *newAddRemoveMoney(void)
 {
-	AddRemoveMoney *a;
-
-	a = makeAddRemoveMoneyFromUIFile();
-	g_signal_connect(a->main, "destroy", G_CALLBACK(destroy), a);
-
-	// Glade doesn't let me set these two
-	gtk_button_set_image(GTK_BUTTON(a->addAmount), gtk_image_new_from_icon_name("list-add-symbolic", GTK_ICON_SIZE_MENU));
-	gtk_button_set_image(GTK_BUTTON(a->removeAmount), gtk_image_new_from_icon_name("list-remove-symbolic", GTK_ICON_SIZE_MENU));
-
-	return a->main;
+	return GTK_WIDGET(g_object_new(AddRemoveMoneyType, NULL));
 }
